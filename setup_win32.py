84b87234926871595a6842f597362c7eb0f100a0
Shestoperd <antuanelli@gmail.com>
2016-06-14 18:52:24 +0000

    Fix setup script for building win32 aceproxy

--- setup_win32.py
+++ setup_win32.py
@@ -7,18 +7,41 @@ Command to make a distro: python setup_win32.py bdist_msi
 By default cx_Freeze created installer will generate shortcuts without working dir that's why plugins will not load.
 To overcome this you need to edit one line in cx_Freeze\windist.py, see guide here:
     http://stackoverflow.com/questions/24195311/how-to-set-shortcut-working-directory-in-cx-freeze-msi-bundle
+change:
+    msilib.add_data(self.db, "Shortcut",
+        [("S_APP_%s" % index, executable.shortcutDir,
+                executable.shortcutName, "TARGETDIR",
+                "[TARGETDIR]%s" % baseName, None, None, None,
+                None, None, None, None)])
+to
+    msilib.add_data(self.db, "Shortcut",
+        [("S_APP_%s" % index, executable.shortcutDir,
+                executable.shortcutName, "TARGETDIR",
+                "[TARGETDIR]%s" % baseName, None, None, None,
+                None, None, None, "TARGETDIR")]) # <--- Working directory.
+
 Hope that will be fixed soon or the author will do much easier way to set working dir.
+
+To fix AttributeError: 'module' object has no attribute 'path' need to edit cx_Freeze\freezer.py, see guide here:
+    https://bitbucket.org/anthony_tuininga/cx_freeze/issues/42/recent-versions-of-gevent-break
+change:
+    import os, imp, sys
+to
+    import imp, sys
+    os = sys.modules['os']
+
+
 '''
 
 from cx_Freeze import setup, Executable
 
 build_exe_options = {"excludes": ["email", "unittest", "aceconfig", "plugins"],
-                     "includes": ["xml.dom.minidom"],
-                     "include_files": ["aceconfig.py", "plugins"],
+                     "includes": ["xml.dom.minidom", "md5"],
+                     "include_files": ["aceconfig.py", "plugins", "acedefconfig.py"],
                      "compressed": True}
 
 setup(name="AceProxy",
-      version="0.9",
+      version="0.9.357",
       description="AceProxy",
       options={"build_exe": build_exe_options},
       executables=[
